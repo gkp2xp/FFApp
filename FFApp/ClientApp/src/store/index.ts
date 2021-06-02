b6c699e63808cfef5bf7a7054bcca2cf416b1781
Georges Akpoly <omdjez@hotmail.com>
2021-06-02 15:16:28 -0400

    code cleanup. removed default template artifacts

--- FFApp/ClientApp/src/store/index.ts
+++ FFApp/ClientApp/src/store/index.ts
@@ -1,11 +1,7 @@
-import * as WeatherForecasts from './WeatherForecasts';
 import * as Investments from './Investments';
-import * as Counter from './Counter';
 
 // The top-level state object
 export interface ApplicationState {
-    counter: Counter.CounterState | undefined;
-    weatherForecasts: WeatherForecasts.WeatherForecastsState | undefined;
     investments: Investments.InvestmentState | undefined;
 }
 
@@ -13,8 +9,6 @@ export interface ApplicationState {
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
-    counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer,
     investments: Investments.reducer,
 };
 
